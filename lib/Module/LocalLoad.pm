c1ab118e8493f2bbdae40d272076302092e0cf54
Magnus Woldrich <trapd00r@trapd00r.se>
2011-03-06 00:44:49 +0100

    LocalLoad.pm: rewrite and add lots of documentation

--- lib/Module/LocalLoad.pm
+++ lib/Module/LocalLoad.pm
@@ -1,14 +1,14 @@
 package Module::LocalLoad;
 use strict;
+use warnings;
 use vars qw($VERSION);
-$VERSION = '0.150';
+$VERSION = '0.170';
 
 use Carp();
 use File::Copy();
 use File::Path();
 
-
-my $PERL5HACKLIB;
+my $PERL_HACK_LIB = $ENV{PERL_HACK_LIB};
 
 sub import {
   my $who = (caller(1))[0];
@@ -16,6 +16,7 @@ sub import {
   {
     no strict 'refs';
     *{"${who}::load"} = *load;
+
   }
 }
 
@@ -23,22 +24,21 @@ sub load {
   my $module = shift or return;
 
   my $who = (caller(1))[0];
-  $PERL5HACKLIB = $ENV{PERL5HACKLIB};
 
-  if(!defined($PERL5HACKLIB)) {
-    Carp::croak("Environment variable \$PERL5HACKLIB not set! Aborting.\n");
+  if(!defined($PERL_HACK_LIB)) {
+    Carp::croak("Environment variable \$PERL_HACK_LIB not set! Aborting.\n");
   }
 
   my $slashed_module = _colon_to_slash( $module );
 
-  if(! -d "$PERL5HACKLIB/$slashed_module") {
-    File::Path::make_path("$PERL5HACKLIB/$slashed_module")
-      or Carp::croak("Cant mkdir $PERL5HACKLIB/$slashed_module: $!\n");
+  if(! -d "$PERL_HACK_LIB/$slashed_module") {
+    File::Path::make_path("$PERL_HACK_LIB/$slashed_module")
+      or Carp::croak("Cant mkdir $PERL_HACK_LIB/$slashed_module: $!\n");
   }
 
   my $found_pm;
   for my $dir_in_inc(@INC) {
-    if($dir_in_inc eq $PERL5HACKLIB) {
+    if($dir_in_inc eq $PERL_HACK_LIB) {
       next;
     }
     if( -f "$dir_in_inc/$slashed_module.pm") {
@@ -51,22 +51,21 @@ sub load {
     Carp::croak("Could not find $module in \@INC\n");
   }
 
-  if(! -f "$PERL5HACKLIB/$slashed_module.pm") {
-    File::Copy::copy($found_pm, "$PERL5HACKLIB/$slashed_module.pm")
-      or croak("Can not copy $found_pm to $PERL5HACKLIB/$slashed_module.pm: $!");
+  if(! -f "$PERL_HACK_LIB/$slashed_module.pm") {
+    File::Copy::copy($found_pm, "$PERL_HACK_LIB/$slashed_module.pm")
+      or Carp::croak(
+      "Can not copy $found_pm to $PERL_HACK_LIB/$slashed_module.pm: $!"
+    );
   }
 
-  unshift(@INC, $PERL5HACKLIB) unless $INC[0] eq $PERL5HACKLIB;
+  unshift(@INC, $PERL_HACK_LIB) unless $INC[0] eq $PERL_HACK_LIB;
 
   eval "require $module";
   $@ ? Carp::croak("Error loading $module: $@\n") : return 1;
 }
 
-sub _colon_to_slash {
-  my $module = shift;
-  $module =~ s{::}{/}g;
-  return $module;
-}
+
+sub _colon_to_slash { return join('/', split(/::/, shift)) }
 
 
 1;
@@ -78,70 +77,73 @@ __END__
 
 =head1 NAME
 
-Module::LocalLoad - create and use a local lib/ for globally installed packages
+Module::LocalLoad - create and use a local lib/ for globally installed modules
 
 =head1 SYNOPSIS
 
-  my $module = 'IO::File';
-  load($module) and printf("%s v%s loaded\n", $module, $module->VERSION);
+  my $module = 'Term::ANSIColor';
+  load($module) and printf("%s v%s loaded\n", $module $module->VERSION);
 
 =head1 DESCRIPTION
 
-You might find yourself in a situation where you need to change something in the
-source of a globally installed package. Doing so directly might not be such a
-good idea, and sometimes not even possible.
+You're debugging your code, and it's still failing even though you're doing
+everything right. You might have misinterpreted the documentation for some
+module you're using, or perhaps it's not doing what it says it should.
+
+Time to take a peek at the inner guts of said module. Change a few things, and
+see if your problem goes away.
+
+Changing code in a globally installed module is not such a great idea. Sometimes
+it's not even possible.
 
 This module will help you set up a temporary local lib/ for the modules that you
 are working on right now. See the L</EXAMPLES> section.
 
 =head1 EXPORTS
 
-=head2 load()
-
-=over 4
-
-=item Arguments:    $package
-
-=item Return value: Boolean
-
-=back
+=head2 load( $package )
 
 When load() is called with a valid, globally installed package name several
-things happen. First, we check if the environment variable C<PERL5HACKLIB> is
+things happen. First, we check if the environment variable C<PERL_HACK_LIB> is
 defined and points to a directory that'll be our new lib/.
-If the directory already contains a copy of the package, we go ahead and load
-it, else we must first copy it.
+If it isnt, we croak, announcing that it needs to be set.
+
+If the directory already contains a copy of the module, we go ahead and load it.
+We don't want our changes to be overwritten everytime we load the module.
+
+Otherwise, we copy the module, if existing in C<@INC>, to C<PERL_HACK_LIB>,
+modify C<@INC> so that C<PERL_HACK_LIB> comes first, and loads it.
 
 =head1 EXAMPLES
 
 You want to muck around in the inner workings of the IO::File module.
 
-  # io-file-hack.pl
+  # load.pl
   use Module::LocalLoad;
 
+  my $m = 'Term::ANSIColor';
+  (my $f = $m) =~ s{::}{/}g;
+  $f .= '.pm';
 
-  my $m = 'IO::File';
-  my $f = $m;
-  $f =~ s{::}{/}g;
-
-  load($m) and printf("%s v%s loaded - %s\n", $m, $m->VERSION, $INC{"$f.pm"});
+  load($m) and printf("%s v%s loaded - %s\n", $m, $m->VERSION, $INC{$f.});
 
 This will produce something like:
 
-  IO::File v1.14 loaded - /tmp/lib/IO/File.pm
+  Term::ANSIColor v3.00 loaded - /tmp/Term/ANSIColor.pm
 
-Next up, go make some changes to /tmp/lib/IO/File.pm . Don't forget to change
-the $VERSION variable!
+Next up, go make some changes to /tmp/Term/ANSIColor.pm .
+Notice the version number reported from ->VERSION:
 
-  vim /tmp/lib/IO/File.pm
+  vim /tmp/Term/ANSIColor.pm
+  perl load.pl
 
-  IO::File v1.14.1 loaded - /tmp/lib/IO/File.pm
+  Term::ANSIColor v3.00_042 loaded - /tmp/Term/ANSIColor.pm
 
 =head1 ENVIRONMENT
 
 =over 4
 
-=item PERL5HACKLIB
+=item PERL_HACK_LIB
 
 =back
 
